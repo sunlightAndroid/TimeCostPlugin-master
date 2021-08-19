8a61b021b763896d73b96b672c3e91b7de4f9461
戈传光 <>
2021-08-19 18:21:23 +0800

    标记一下输出内容

--- buildSrc/src/main/groovy/me/eric/costTime/TimeConsumingClassVisitor.groovy
+++ buildSrc/src/main/groovy/me/eric/costTime/TimeConsumingClassVisitor.groovy
@@ -16,10 +16,15 @@ class TimeConsumingClassVisitor extends ClassVisitor {
     @Override
     void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
         cv.visit(version, access, name, signature, superName, interfaces)
+        System.out.println("》》》》》 TimeConsumingClassVisitor:" + "  name:" + name + " signature:" + signature + " superName:" + superName)
+        // 》》》》》 TimeConsumingClassVisitor:  name:me/eric/timeCost/MainActivity signature:null superName:androidx/appcompat/app/AppCompatActivity
     }
 
     @Override
     MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
+        System.out.println("》》》》》 TimeConsumingClassVisitor:" + "  MethodName:" + name + " desc:" + desc + " signature:" + signature)
+        // 》》》》》 TimeConsumingClassVisitor:  MethodName:testTime desc:()V signature:null
+
         MethodVisitor mv = cv.visitMethod(access, name, desc, signature, exceptions)
         if ("<init>" != name && "testTime" == name && mv != null) {
             mv = new TimeConsumingMethodVisitor(mv)
